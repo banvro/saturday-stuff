ea324dfaa4d4aa438dc8c9734f4802c0e3ee36e8
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2024-03-02 14:49:37 +0530

    Create core-python-first-class.py

---
+++ core-python-first-class.py
@@ -0,0 +1,140 @@
+Python history:::
+        
+        1980....
+        
+        abc lnguage
+        objctice
+        c
+        unix
+        .
+        ..
+
+
+Guido Van Rossum
+
+
+1980 ------ 1991
+
+1991---> python
+            python 0.9.0
+            
+            
+            
+2000 ---> python 2
+
+# 2020
+
+
+
+2008 ---- python 3
+
+    python 3.12.2
+    
+
+
+
+Unique Features:
+    1) Simple
+    2) Gernal Perpose
+    3) high level
+    4) objecct oriented
+    5) scrippting
+    6) Interpreated
+    7) Dynmically Typed
+    8) Case senstive...
+
+
+# 1) Simple
+
+8219836118
+
+adding two number using c:
+    
+# include <stdio.h>
+# include < conio.h>
+
+int main(){
+    int a;
+    int b;
+    int c;
+    a = 10;
+    b = 20;
+    c = a + b;
+    printf("%d", c);
+    
+    return 0
+}
+
+
+# python adatinonnn
+
+a = 10
+b = 20
+c = a + b
+print(c)
+
+
+
+2) Gernal Perpose:
+    1) Web devlopment
+            1) flask
+            2) django
+            
+    2) API devlopment
+            DRF
+        
+    3) Desktop applications
+        PyQT5
+        
+    4) AI 
+
+
+
+3) Languages...:
+    1) High Level Language
+    
+    2) Low Level Langauges
+        machine language/ binary language
+        
+        01
+
+
+4) interpreater       ////     complier  
+
+
+
+5)
+
+
+5) scrippting:
+    
+a = 100
+b = 200
+c = a + b
+print(c)
+
+
+
+
+7) Dynmically Typed
+
+a = "hjvjvjvj"
+
+
+8) Case senstive...
+
+age = 20
+
+Age = 67
+
+
+
+CREATE DATABASE HELLO;
+
+create database hello;
+
+a = 10
+
+A = 90
+
+print(A)
